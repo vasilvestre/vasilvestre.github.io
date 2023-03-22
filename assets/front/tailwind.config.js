977d4a274b41b886c8e03924a6881a1628f6ddfe
Valentin Silvestre <vsilvestre.pro@gmail.com>
2023-03-22 15:18:09 +0100

    Adding site foundations
    --

---
+++ assets/front/tailwind.config.js
@@ -0,0 +1,26 @@
+const Encore = require('@symfony/webpack-encore');
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        'prose': '70ch',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  corePlugins: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+  purge: Encore.isProduction(),
+}
