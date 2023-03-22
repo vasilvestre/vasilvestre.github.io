c76108c0e8d371831594dd58a20f1a2dcf91c524
Valentin Silvestre <vsilvestre.pro@gmail.com>
2023-03-22 15:18:47 +0100

    Last version of website design

--- assets/front/tailwind.config.js
+++ assets/front/tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'green-tree': '#266149',
+        beige: '#FFFCE8',
+        'dark-beige': '#fefee2',
+        'light-green': '#BDF4B2',
+        green: '#597354',
+        blue: '#25626e',
+        'dark-blue': '#1C4952',
+      },
+      backgroundImage: {
+        what: "url('./images/texture-what.jpg')",
+      }
     },
   },
   variants: {},
